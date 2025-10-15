9736cf0dee27876f10e03335d43c3139b4331783
SergioBejarano <sergio2018.bejarano>
2025-10-15 10:16:46 -0500

    first commit

---
+++ src/main/java/co/edu/escuelaing/lambda/MathServices.java
@@ -0,0 +1,11 @@
+package co.edu.escuelaing.lambda;
+
+/**
+ *
+ * @author sergio.bejarano-r
+ */
+public class MathServices {
+    public static Integer square(Integer i) {
+        return i * i;
+    }
+}
